2bbfb40abc84222a8bfab57f6ef7cf2e34bb42ab
Pedro Henrique <89044375+PedroHSJ@users.noreply.github.com>
2025-06-03 18:31:38 +0000

    corrigindo redirect

--- src/components/form/login-form.tsx
+++ src/components/form/login-form.tsx
@@ -17,7 +17,7 @@ export function LoginForm({
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`,
+      redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
     if (error) alert(error.message);
