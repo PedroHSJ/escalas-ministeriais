ebe132f2d838e57b4d7a2c8f846017d114b90237
Pedro Henrique <89044375+PedroHSJ@users.noreply.github.com>
2025-06-03 18:38:30 +0000

    CRIANDO ROUTE TS

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get('code');
+
+  if (code) {
+    const cookieStore = cookies();
+    
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          async get(name: string) {
+            return (await cookieStore).get(name)?.value;
+          },
+          async set(name: string, value: string, options: any) {
+            (await cookieStore).set({ name, value, ...options });
+          },
+          async remove(name: string, options: any) {
+            (await cookieStore).set({ name, value: '', ...options });
+          },
+        },
+      }
+    );
+
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(new URL('/dashboard', request.url));
+}
