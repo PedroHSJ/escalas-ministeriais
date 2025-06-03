0e54d2e82b36a8a383725305e41c91adbdaaa125
Pedro Henrique <89044375+PedroHSJ@users.noreply.github.com>
2025-06-03 19:35:23 +0000

    Corrije import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import updateSession from "./lib/supabse/middleware";
+import {updateSession} from "./lib/supabse/middleware";
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
