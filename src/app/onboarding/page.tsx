0982fcf9d86c398454211a161945d4c8da4464f7
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2025-08-18 17:42:30 -0300

    refactor: simplify user ID handling in onboarding page

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,44 +1,30 @@
 "use client";
 import { OrganizationWizard } from "@/components/wizard/OrganizationWizard";
+import { useAuth } from "@/contexts/AuthContext";
 import { supabase } from "@/lib/supabaseClient";
 import { redirect, useRouter } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
 
-export default function Page(){
-    const router = useRouter();
+export default function Page() {
+  const { userId } = useAuth();
 
-      const [userId, setUserId] = useState("");
-      const fetchSession = async () => {
-        const { data } = await supabase.auth.getSession();
-        if (data.session?.user) {
-          setUserId(data.session.user.id);
-        } else {
-         setUserId("d7e39c07-f7e4-4065-8e3a-aac5ccb02f1b"); // For testing purposes, using a static user ID    
-        }
-      };
-    
-      useEffect(() => {
-        fetchSession();
-      }, []);
-
-      const handleWizardComplete = () =>{
-        redirect("/dashboard");
+  const handleWizardComplete = () => {
+    redirect("/dashboard");
+  };
+  return (
+    <Suspense
+      fallback={
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+        </div>
       }
-    return(
-        <Suspense fallback={
-            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                <div className="bg-muted/50 aspect-video rounded-xl" />
-                </div>
-                <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-            </div>
-            }>
-        <OrganizationWizard 
-         userId={userId} 
-         onComplete={handleWizardComplete}
-       />
-        </Suspense>
-    )
+    >
+      <OrganizationWizard userId={userId} onComplete={handleWizardComplete} />
+    </Suspense>
+  );
 }
